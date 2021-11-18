811ffd0da4951f4ae10a5661fe78ece4861e0e51
zgy <qbjooo@qq.com>
2021-11-18 08:53:24 +0000

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from glob import glob
 if __name__ == "__main__":
     setup(
         name='cpm_kernels',
-        version='1.0.4',
+        version='1.0.5',
         packages=find_packages(),
         description='CPM CUDA kernels',
         long_description=open("./README.md", 'r').read(),
